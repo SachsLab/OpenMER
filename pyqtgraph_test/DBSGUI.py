e8f1ff6abab1728be32ec4bd6cc5abad6664359d
nikgetas <r930710@gmail.com>
2016-08-23 13:32:26 -0400

    New push. clean up the code and add the addChannel function. But the index of channel to plot is disorder.

--- pyqtgraph_test/DBSGUI.py
+++ pyqtgraph_test/DBSGUI.py
@@ -1,5 +1,4 @@
 import sys
-import time
 import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
@@ -31,133 +30,172 @@ class MyGUI(QtGui.QMainWindow):
     def setupUI(self):
 
         self.dock_area = DockArea(self)
+
         self.setCentralWidget(self.dock_area)
 
-        self.resize(1200, 900)
+        self.resize(1200, 621)
+        # Set menubar & whole widget appearance
+        self.myMenubar = QtGui.QMenuBar(self)
+        self.myMenubar.setGeometry(0,0,1200,21)
+        self.myMenubar.setObjectName("MenuBar")
+        fileMenu = self.myMenubar.addMenu('File')
+        addChannelAction = QtGui.QAction('Add a channel',self)
+        #selectChannelAction = QtGui.QAction('Select Channel',self)
+        fileMenu.addAction(addChannelAction)
+        channelList = fileMenu.addMenu('Channel List')
+        addChannelAction.triggered.connect(self.AddChannelToPlot)
+        self.ChannelList = QtGui.QComboBox()
+        for ch_id in range(NCHANNELS):
+            self.ChannelList.addItem("{}".format(ch_id+1))
+
         self.setWindowTitle('Neuroport DBS')
         self.statusBar().showMessage('Ready...')
 
         #TODO: Create NUNITS pens
-        self.mypens = [pg.mkPen(cosmetic=False, width=0.5)]
+        self.mypens = [pg.mkPen( width=0, color='y')]
+        #self.mypens[0] = pg.mkPen(color='y')
         # mybrush = pg.mkBrush(QtGui.QBrush(QtGui.QColor(QtCore.Qt.blue), style=QtCore.Qt.VerPattern))
-        mybrush = None
+        self.mybrush = None
 
-        self.my_spks = []
-        self.my_raws = []
-        self.my_rasters = []
-        n_chans_to_plot = 1
-        self.spk_buffer = np.empty((n_chans_to_plot, 0))
-        self.raw_buffer = np.empty((n_chans_to_plot, 0))
-        self.raster_buffer = [np.empty((1,)) for x in range(n_chans_to_plot)]
-        for ch_ix in range(n_chans_to_plot):
-            continuous_dock = Dock("Continuous " + str(ch_ix), size=(900, 300))
-            continuous_layout = pg.LayoutWidget()
-            combobox = QtGui.QComboBox()
-            for ch_id in range(NCHANNELS):
-                combobox.addItem("{}".format(ch_id+1))
-            spk_plot = pg.PlotWidget(name="SPK")
-            spk_plot.plotItem.plot([])
-            raw_plot = pg.PlotWidget(name="RAW")
-            raw_plot.plotItem.plot([])
-            continuous_layout.addWidget(combobox)
-            continuous_layout.addWidget(spk_plot)
-            continuous_layout.addWidget(raw_plot)
-            continuous_dock.addWidget(continuous_layout)
-            self.dock_area.addDock(continuous_dock, position='bottom')
-            self.my_spks.append(spk_plot)
-            self.my_raws.append(raw_plot)
+        self.painter = QtGui.QPainter()
+        #self.painter.setPen()
+        #poly = QtGui.QPolygonF([QtCore.QPoint(0, 0), QtCore.QPoint(0, 1)])
 
-            raster_dock = Dock("Raster " + str(ch_ix), size=(300, 300))
-            raster_layout = pg.LayoutWidget()
-            combo2 = QtGui.QComboBox()
-            for ch_id in range(NCHANNELS):
-                combo2.addItem("{}".format(ch_id+1))
-            raster_layout.addWidget(combo2)
-            raster_item = pg.GraphItem(symbolPen=self.mypens[0], symbolBrush=mybrush)
-            raster_plot = pg.PlotWidget()
-            raster_plot.setLimits(xMin=0, xMax=1, yMin=0, yMax=1, minXRange=1.0, maxXRange=1.0, minYRange=1.0,
-                                  maxYRange=1.0)
-            raster_plot.addItem(raster_item)
-            raster_layout.addWidget(raster_plot)
-            raster_dock.addWidget(raster_layout)
-            self.my_rasters.append(raster_item)  # TODO: Get rid of me when I figure out how to search for GraphItem
-            #raster_dock.hideTitleBar()
-            self.dock_area.addDock(raster_dock, position='right', relativeTo=continuous_dock)
+
+        self.ticks = QtGui.QPainterPath()
+        #self.ticks.addPolygon(poly)
+        self.ticks.moveTo(0.0, 0.0)
+        self.ticks.lineTo(0.0, 1.0)
+        self.ticks.closeSubpath()
+
+        #self.line = QtCore.QLineF(0,0,0,1)
+        #self.painter.fillPath(self.ticks, QColor=(255,0,0))
+
+        # Setting spikes, raw and raster data buffers
+        # self.my_spks = []
+        # self.my_raws = []
+        # self.my_rasters = []
+        #self.n_chans_to_plot = 1
+        self.spk_buffer = np.empty((0, 0))
+        self.raw_buffer = np.empty((0, 0))
+        self.raster_buffer = [np.empty((1,)), ]
+
+    def AddChannelToPlot(self):
+        totalNumber = self.dock_area.findAll()
+        dock_index = len(totalNumber[0])
+        print(totalNumber[0])
+        if len(totalNumber[0]) is 0:
+            dock_index = 1
+        continuous_dock = Dock("Continuous " + str(dock_index), size=(900, 200))
+
+        continuous_layout = pg.LayoutWidget()
+        combobox = QtGui.QComboBox()
+        for ch_id in range(NCHANNELS):
+            combobox.addItem("{}".format(ch_id+1))
+        spk_plot = pg.PlotWidget(name="SPK")
+        spk_plot.plotItem.plot([])
+        raw_plot = pg.PlotWidget(name="RAW")
+        raw_plot.plotItem.plot([])
+        continuous_layout.addWidget(combobox)
+        continuous_layout.addWidget(spk_plot)
+        continuous_layout.addWidget(raw_plot)
+        continuous_dock.addWidget(continuous_layout)
+        self.dock_area.addDock(continuous_dock, position='bottom')
+        self.spk_buffer = np.concatenate((self.spk_buffer, np.nan*np.ones((1,self.spk_buffer.shape[1]))), axis=0)
+
+        raster_dock = Dock("Raster " + str(dock_index), size=(300, 200)) #Figure out how to show the current index of raster
+        raster_layout = pg.LayoutWidget()
+        combo2 = QtGui.QComboBox()
+        for ch_id in range(NCHANNELS):
+            combo2.addItem("{}".format(ch_id+1))
+        raster_layout.addWidget(combo2)
+
+        raster_item = pg.GraphItem(symbolPen=self.mypens[0], symbolBrush=self.mybrush, symbol='s')
+
+        raster_plot = pg.PlotWidget(name="RASTER")
+        raster_plot.setLimits(xMin=0, xMax=1, yMin=0, yMax=1, minXRange=1.0, maxXRange=1.0, minYRange=1.0,
+                              maxYRange=1.0)
+        raster_plot.addItem(raster_item)
+        raster_layout.addWidget(raster_plot)
+        raster_dock.addWidget(raster_layout)
+        self.dock_area.addDock(raster_dock, position='right', relativeTo=continuous_dock)
+        self.raster_buffer.append(np.empty((1,)))
 
     def setupNeuroport(self):
         self.cbsdkConn = cbsdkConnection()
 
     def update(self):
+
+        dock_info = []
+        for dock_key in self.dock_area.docks.keys():
+            dock_type, dock_num = dock_key.split(' ')
+            this_dock = self.dock_area.docks[dock_key]
+            combo_box = this_dock.findChild(QtGui.QComboBox)
+            chan_ix = int(combo_box.currentText())
+            dockplot = this_dock.findChild(name="RASTER" if dock_type == 'Raster' else "SPK")  #?
+            #TODO: Search for children with name == "RASTER" or name == "SPK", set to dockplot
+            dock_info.append({'type': dock_type, 'num': int(dock_num), 'chan': chan_ix, 'plot': dockplot})
+
         # Get event timestamps
         timestamps, ts_time = self.cbsdkConn.get_event_data()
         timestamp_chans = [x[0] for x in timestamps]
-        # Buffer updated inside dock loop
 
         # Get continuous
         contdat, cont_time = self.cbsdkConn.get_continuous_data()
         cont_chans = [x[0] for x in contdat]
         first_dat = contdat[0][1]
         n_samples_to_add = first_dat.size
-        self.spk_buffer = np.concatenate((self.spk_buffer, np.nan*np.ones((self.spk_buffer.shape[0], n_samples_to_add), dtype=first_dat.dtype)), axis=1)
-
-        # self.data1[:-1] = self.data1[1:]  # shift data in the array one sample left
-        # self.data1[-1] = np.random.normal()
-        # self.ptr1 += 1
-        # self.curve1.setData(self.data1)
-        # self.curve1.setPos(self.ptr1, 0)
-
+        self.spk_buffer = np.concatenate((self.spk_buffer, np.nan*np.ones((self.spk_buffer.shape[0], n_samples_to_add),
+                                                                          dtype=first_dat.dtype)), axis=1)
+        # Add relevant data to spk_buffer
         spk_index = -1
         gi_index = -1  # FIXME
-        for dock_key in self.dock_area.docks.keys():
-            this_dock = self.dock_area.docks[dock_key]
-            dock_type = dock_key.split(' ')[0]
-            combo_box = this_dock.findChild(QtGui.QComboBox)
-            chan_ix = int(combo_box.currentText())
-            if dock_type == 'Raster':
-                gi_index += 1  # FIXME
+        for dinf in dock_info:
+            if dinf['type'] == 'Continuous':
                 try:
-                    this_ix = timestamp_chans.index(chan_ix)
-                    this_timestamps = timestamps[this_ix][1]['timestamps']
-                except ValueError:
-                    this_timestamps = [[]]
-
-                # for unit_ix in range(NUNITS):
-                unit_ix = 0
-                self.raster_buffer[gi_index] = np.append(self.raster_buffer[gi_index], this_timestamps[unit_ix])
-                ts = (self.raster_buffer[gi_index] - ts_time) / SAMPLERATE
-
-                # Only keep last 5 seconds
-                keep_mask = ts > -5
-                self.raster_buffer[gi_index] = self.raster_buffer[gi_index][keep_mask]  # Trim buffer (samples)
-                ts = ts[keep_mask]  # Trim timestamps (seconds)
-
-                xpos = np.mod(ts, 1)
-                ypos = -np.ceil(ts)/5+0.1
-                positions = np.vstack((xpos, ypos)).T
-
-                gi = self.my_rasters[gi_index]
-                gi.setData(pos=positions, symbolPen=self.mypens[0])
-
-            elif dock_type == 'Continuous':
-                spk_index += 1  #FIXME
-                try:
-                    this_ix = cont_chans.index(chan_ix)
-                    self.spk_buffer[this_ix, -n_samples_to_add:] = contdat[this_ix][1]
+                    buf_ix = dinf['num']
+                    dat_ix = dinf['chan']
+                    self.spk_buffer[buf_ix-1, -n_samples_to_add:] = contdat[cont_chans.index(dat_ix)][1]
                     tvec = np.arange(-self.spk_buffer.shape[1], 0) / SAMPLERATE
 
+                    # Shrink spk_buffer to time >= -1
                     sample_mask = tvec >= -1
                     tvec = tvec[sample_mask]
                     self.spk_buffer = self.spk_buffer[:, sample_mask]
 
-                    #pw = this_dock.children()[2].children()[1].children()[2]
-                    pi = self.my_spks[spk_index].getPlotItem()
+                    pi = dinf['plot'].getPlotItem()
                     dataItems = pi.listDataItems()
-                    dataItems[0].setData(self.spk_buffer.ravel(), x=tvec)
+                    dataItems[0].setData(self.spk_buffer[dinf['num']-1, :].ravel(), x=tvec)
                 except ValueError:
                     pass
 
+            elif dinf['type'] == 'Raster':
+                try:
+                    this_ix = timestamp_chans.index(dinf['chan'])
+                    this_timestamps = timestamps[this_ix][1]['timestamps']
+                    raster_buf_ix = dinf['num']
+                    raster_dat_ix = dinf['chan']
+                except ValueError:
+                    this_timestamps = [[]]
+
+                #self.raster_buffer[raster_buf_ix-1] = np.append(self.raster_buffer[gi_index], this_timestamps[unit_ix])
+                self.raster_buffer[raster_buf_ix-1] = this_timestamps[0]
+                ts = (self.raster_buffer[raster_buf_ix-1] - ts_time) / SAMPLERATE
+
+                # Only keep last 5 seconds
+                keep_mask = ts > -5
+                self.raster_buffer[raster_buf_ix-1] = self.raster_buffer[raster_buf_ix-1][keep_mask]  # Trim buffer (samples)
+                ts = ts[keep_mask]  # Trim timestamps (seconds)
+
+                xpos = np.mod(ts, 1)
+                ypos = -np.ceil(ts) / 5 + 0.1
+                positions = np.vstack((xpos, ypos)).T
+
+                dinf['plot'].setData(pos=positions, symbolPen=self.mypens[0])
+
 
 class cbsdkConnection(object):
+
     def __init__(self, inst_addr='192.168.137.128', inst_port=51001, client_port=51002,
                  client_addr='192.168.137.255' if sys.platform == 'linux2' else '255.255.255.255',
                  receive_buffer_size=(4096 * 1536) if sys.platform == 'win32' else (4096 * 1536)):
@@ -286,8 +324,6 @@ class Spikes:
 
 
 if __name__ == '__main__':
-    #spike1 = Spikes([4,5,6,7,8])
-    #spike1.start()
 
     qapp = QtGui.QApplication(sys.argv)
     aw = MyGUI()
