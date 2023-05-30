e985eb340db2d58bbb397f6cd6dcc61b9490ab13
Chadwick Boulay <chadwick.boulay@gmail.com>
2023-05-29 22:18:08 -0400

    Re-add DDU detection lost in merge.

--- neuroport_dbs/depth_source/base.py
+++ neuroport_dbs/depth_source/base.py
@@ -5,7 +5,7 @@ from ..settings import parse_ini_try_numeric
 class MerDepthSource:
     def __init__(self, scoped_settings: QtCore.QSettings):
         # scale_factor should be 0.001 for FHC DDU V2, 1.0 otherwise.
-        self._scale_factor = parse_ini_try_numeric(scoped_settings, 'scale_factor') or 1.0
+        self._scale_factor = parse_ini_try_numeric(scoped_settings, 'scale_factor') or None
         self.do_open()
 
     def do_open(self):
--- neuroport_dbs/depth_source/fhc.py
+++ neuroport_dbs/depth_source/fhc.py
@@ -1,3 +1,5 @@
+import re
+
 from qtpy import QtCore
 import serial
 import serial.tools.list_ports
@@ -11,7 +13,18 @@ class FHCSerial(MerDepthSource):
         super().__init__(scoped_settings)
         self._baudrate = parse_ini_try_numeric(scoped_settings, 'baudrate') or 19200
         self._com_port = scoped_settings.value("com_port")
-        self.ser = serial.Serial()
+        self.ser = serial.Serial(timeout=1)
+        self._is_v2 = False
+
+    @property
+    def is_v2(self):
+        return self._is_v2
+
+    @is_v2.setter
+    def is_v2(self, value):
+        self._is_v2 = value
+        self._scale_factor = 0.001 if value else 1.0
+        self.doubleSpinBox_offset.setValue(60.00 if value else 0.00)
 
     def do_open(self):
         super().do_open()
@@ -21,8 +34,22 @@ class FHCSerial(MerDepthSource):
         if not self.ser.is_open:
             self.ser.port = self._com_port
             try:
-                self.ser.open()  # TODO: Add timeout; Add error.
-                self.ser.write('AXON+\r'.encode())
+                self.ser.open()  # TODO: Add error.
+                # Quiet transmission for a minute
+                self.ser.write("AXON-\r".encode())
+                self.ser.write("V\r".encode())
+                # readlines will capture until timeout
+                pattern = "([0-9]+\.[0-9]+)"
+                for line in self.ser.readlines():
+                    match = re.search(pattern, line.decode("utf-8").strip())
+                    if match is not None:
+                        v = match.group()  # e.g., "2.20"
+                        v_maj = int(v.split(".")[0])
+                        self.is_v2 = v_maj >= 2
+                        break
+                # Resume transmission.
+                self.ser.write("AXON+\r".encode())
+                _ = self.ser.readline()  # Clear out the first response to AXON+
             except serial.serialutil.SerialException:
                 print("Could not open serial port")
 
@@ -34,7 +61,7 @@ class FHCSerial(MerDepthSource):
             in_str = self.ser.readline().decode('utf-8').strip()
             if in_str:
                 try:
-                    raw_value = float(in_str) * self._scale_factor
+                    raw_value = float(in_str) * (self._scale_factor or 1.0)
                     return raw_value
 
                 except ValueError:
